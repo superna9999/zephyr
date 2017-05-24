a644a4d2c4ca9059e282fded5047a2aa7c030e8f
Neil Armstrong <narmstrong@baylibre.com>
2017-05-24 16:33:36 +0200

    Add UART on PA9/PA10 STM32L0 defines

---
+++ drivers/pinmux/stm32/pinmux_stm32l0x.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ * Copyright (c) 2016 BayLibre, SAS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STM32L0X_PINMUX_H_
+#define _STM32L0X_PINMUX_H_
+
+/**
+ * @file Header for STM32L0X pin multiplexing helper
+ */
+
+/* Port A */
+#define STM32L0X_PINMUX_FUNC_PA9_USART1_TX STM32_PINMUX_FUNC_ALT_4
+#define STM32L0X_PINMUX_FUNC_PA10_USART1_RX STM32_PINMUX_FUNC_ALT_4
+
+#endif /* _STM32L0X_PINMUX_H_ */
