f10c340bb585897029a21ed04faf6ae4e7593f5e
Neil Armstrong <narmstrong@baylibre.com>
2017-11-30 17:39:32 +0100

    i2c: stm32_v2: implement slave support
    This patch adds I2C Slave support conforming to the syscalls and funcs,
    only for the STM32 V2 I2C Driver for the moment.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- drivers/i2c/i2c_ll_stm32.h
+++ drivers/i2c/i2c_ll_stm32.h
@@ -40,6 +40,11 @@ struct i2c_stm32_data {
 		unsigned int len;
 		u8_t *buf;
 	} current;
+#ifdef CONFIG_I2C_SLAVE
+	const struct i2c_slave_api *slave_funcs;
+	void *slave_priv;
+	bool slave_attached;
+#endif 
 };
 
 s32_t stm32_i2c_msg_write(struct device *dev, struct i2c_msg *msg, u8_t *flg,
@@ -47,9 +52,19 @@ s32_t stm32_i2c_msg_write(struct device *dev, struct i2c_msg *msg, u8_t *flg,
 s32_t stm32_i2c_msg_read(struct device *dev, struct i2c_msg *msg, u8_t *flg,
 			 u16_t sadr);
 s32_t stm32_i2c_configure_timing(struct device *dev, u32_t clk);
+int i2c_stm32_runtime_configure(struct device *dev, u32_t config);
 
 void stm32_i2c_event_isr(void *arg);
 void stm32_i2c_error_isr(void *arg);
+
+#if defined(CONFIG_I2C_SLAVE) && defined(CONFIG_I2C_STM32_V2)
+bool i2c_stm32_slave_is_supported(struct device *dev);
+int i2c_stm32_slave_attach(struct device *dev, u8_t address,
+			   const struct i2c_slave_api *funcs,
+			   void *priv);
+int i2c_stm32_slave_detach(struct device *dev, u8_t address,
+			   void *priv);
+#endif
 
 #define DEV_DATA(dev) ((struct i2c_stm32_data * const)(dev)->driver_data)
 #define DEV_CFG(dev)	\
